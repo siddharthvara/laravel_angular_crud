4783f2a1299395d5d2b4b6690147256978a4bc79
siddharth vara <siddharth.vara@gmail.com>
2018-05-31 18:32:53 +0530

    change database path

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'sql12.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laraveldemo'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql12240731'),
+            'username' => env('DB_USERNAME', 'sql12240731'),
+            'password' => env('DB_PASSWORD', 'HChjIh3DWu'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
