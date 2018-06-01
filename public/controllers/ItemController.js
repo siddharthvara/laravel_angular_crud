e6c3bd7af805acc80f3b92625f2c4d2f8f48a17b
siddharth vara <siddharth.vara@gmail.com>
2018-06-01 16:49:15 +0530

    implement email validation.

--- public/controllers/ItemController.js
+++ public/controllers/ItemController.js
@@ -60,7 +60,7 @@ app.controller('ItemController', function(dataFactory,$scope,$http){
 
   $scope.edit = function(id){
     $scope.loading = true;
-    console.log($scope.loading);
+    //console.log($scope.loading);
     dataFactory.httpRequest('students/'+id+'/edit').then(function(data) {
     	console.log(data);
         $scope.form = data;
